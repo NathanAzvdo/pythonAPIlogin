ef6966c96e44aedf9c8bc6591f7f6149bba6a01f
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-05-30 10:56:18 -0300

    remove static

--- app/api.py
+++ app/api.py
@@ -1,4 +1,3 @@
-from starlette.staticfiles import StaticFiles
 from pathlib import Path
 from fastapi import FastAPI
 from sqlalchemy.orm import Session, declarative_base
@@ -11,4 +10,3 @@ app = FastAPI()
 app.include_router(users.router)
 
 PROJECT_ROOT = Path(__file__).parent.parent
-app.mount('/', StaticFiles(directory=PROJECT_ROOT / 'static', html=True))
---
+++ tests/test_user_controller.py
@@ -0,0 +1,3 @@
+from app.controllers.user_controller import *
+
+test_create_user()
